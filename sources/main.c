d9f222e04cef7cb8ceb55acf2b929928b81d6d9c
Sangmin Seo <saseo@c3r1s4.42seoul.kr>
2023-06-01 17:16:16 +0900

    style : main var malloc change

--- sources/main.c
+++ sources/main.c
@@ -2,18 +2,15 @@
 
 int	main(int argc, char **argv)
 {
-	t_vars	*vars;
+	t_vars	vars;
 
-	vars = (t_vars *) malloc(sizeof(t_vars));
-	if (!vars)
-		exit(0);
-	vars->mlx = mlx_init();
-	vars->win = mlx_new_window(vars->mlx, WIDTH, HEIGHT, "fractol");
-	vars->img.image = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
-	color_image(&(vars->img), vars);
-	mlx_key_hook(vars->win, key_hook_esc, vars);
-	mlx_hook(vars->win, 17, 0L, destroy_window, vars);
-	mlx_loop(vars->mlx);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, "miniRT");
+	vars.img.image = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
+	color_image(&(vars.img), &vars);
+	mlx_key_hook(vars.win, key_hook_esc, &vars);
+	mlx_hook(vars.win, 17, 0L, destroy_window, &vars);
+	mlx_loop(vars.mlx);
 	return (0);
 }
 
