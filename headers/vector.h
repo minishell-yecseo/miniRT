38986471badbbf41436221d2e7bef2edf61352b8
Nam Yechan <chanmias0318@gmail.com>
2023-06-01 16:24:21 +0900

    feat : vector & vector basic functions

---
+++ headers/vector.h
@@ -0,0 +1,25 @@
+#ifndef VECTOR_H
+# define VECTOR_H
+
+# include <stdio.h>
+# include <math.h>
+# include <stdlib.h>
+
+typedef struct s_vector
+{
+	double	x;
+	double	y;
+	double	z;
+}	t_vector;
+
+t_vector	vector(double x, double y, double z);
+void		vec_set_value(t_vector *vec, double x, double y, double z);
+double		vec_len_square(t_vector vec);
+double		vec_len(t_vector vec);
+t_vector	vec_add(t_vector vec1, t_vector vec2);
+t_vector	vec_sub(t_vector vec1, t_vector vec2);
+double		vec_dot(t_vector vec1, t_vector vec2);
+t_vector	vec_cross(t_vector vec1, t_vector vec2);
+t_vector	vec_unit(t_vector vec);
+
+#endif
---
+++ sources/vector_func.c
@@ -0,0 +1,81 @@
+#include "vector.h"
+
+t_vector	vector(double x, double y, double z)
+{
+	t_vector	vec;
+
+	vec.x = x;
+	vec.y = y;
+	vec.z = z;
+	return (vec);
+}
+
+void	vec_set_value(t_vector *vec, double x, double y, double z)
+{
+	vec->x = x;
+	vec->y = y;
+	vec->z = z;
+}
+
+double	vec_len_square(t_vector vec)
+{
+	return (vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+}
+
+double	vec_len(t_vector vec)
+{
+	return (sqrt(vec_len_square(vec)));
+}
+
+t_vector	vec_add(t_vector vec1, t_vector vec2)
+{
+	t_vector	vec;
+
+	vec.x = vec1.x + vec2.x;
+	vec.y = vec1.y + vec2.y;
+	vec.z = vec1.z + vec2.z;
+	return (vec);
+}
+
+t_vector	vec_sub(t_vector vec1, t_vector vec2)
+{
+	t_vector	vec;
+
+	vec.x = vec1.x - vec2.x;
+	vec.y = vec1.y - vec2.y;
+	vec.z = vec1.z - vec2.z;
+	return (vec);
+}
+
+// 내적 
+double	vec_dot(t_vector vec1, t_vector vec2)
+{
+	return (vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z);
+}
+
+// 외적
+t_vector	vec_cross(t_vector vec1, t_vector vec2)
+{
+	t_vector	vec;
+
+	vec.x = vec1.y * vec2.z - vec1.z * vec2.y;
+	vec.y = vec1.z * vec2.x - vec1.x * vec2.z;
+	vec.z = vec1.x * vec2.y - vec1.y * vec2.x;
+	return (vec);
+}
+
+t_vector	vec_unit(t_vector vec)
+{
+	double	len;
+
+	len = vec_len(vec);
+	if (len == 0)
+	{
+		printf("Error\n:Devider is 0");
+		exit(0);
+	}
+	vec.x /= len;
+	vec.y /= len;
+	vec.z /= len;
+	return (vec);
+}
