1d1ed06bc1954b7f94423d0de8262f500347b9a1
Nam Yechan <chanmias0318@gmail.com>
2023-06-07 16:05:25 +0900

    ADD : camera dir & fov

--- sources/ray.c
+++ sources/ray.c
@@ -1,4 +1,5 @@
 #include "ray.h"
+#include "math.h"
 
 t_ray	ray(t_vector origin, t_vector dir)
 {
@@ -20,13 +21,25 @@ t_vector	ray_at(t_ray ray, double t)
 t_camera	camera(t_vector origin)
 {
 	t_camera cam;
+	t_vector tmp;
 
-	cam.viewport_h = 2;
-	cam.viewport_w = cam.viewport_h * WIDTH / HEIGHT;
+	cam.dir = vector(0, 0.5, -1);
+	cam.fov = 70;
 	cam.focal_len = 1;
+	cam.viewport_h = 2 * tan(((cam.fov / 2) * (M_PI / 180)));
+	cam.viewport_w = cam.viewport_h * WIDTH / HEIGHT;
+	printf("%f\n", cam.focal_len);
 	cam.origin = origin;
-	cam.horizontal = vector(cam.viewport_w, 0, 0);
-	cam.vertical = vector(0, cam.viewport_h, 0);
-    cam.lower_left_corner = vec_sub(cam.origin, vec_add(vec_add(vec_mul(cam.horizontal, 0.5), vec_mul(cam.vertical, 0.5)), vector(0, 0, cam.focal_len)));
+	if (vec_len(vec_cross(cam.dir, vector(0, 1, 0))))
+		cam.right = vec_mul(vec_unit(vec_cross(cam.dir, vector(0, 1, 0))), cam.viewport_w);
+	else
+		cam.right = vec_mul(vec_unit(vec_cross(cam.dir, vector(0, 0, -1))), cam.viewport_w);
+	cam.up = vec_mul(vec_unit(vec_cross(cam.right, cam.dir)), cam.viewport_h);
+    tmp = vec_add(cam.origin, vec_mul(cam.dir, cam.focal_len));
+	tmp = vec_add(tmp, vec_mul(cam.right, -0.5));
+	tmp = vec_add(tmp, vec_mul(cam.up, -0.5));
+	cam.lower_left_corner = tmp;
+
+	printf("%f %f %f", cam.lower_left_corner.x, cam.lower_left_corner.y, cam.lower_left_corner.z);
 	return (cam);
 }
