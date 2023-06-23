85dbec71a51b766519ef2c50127f3077087275e9
Nam Yechan <chanmias0318@gmail.com>
2023-06-23 12:10:00 +0900

    norminette : headers

--- headers/args.h
+++ headers/args.h
@@ -1,5 +1,5 @@
-#ifndef ARGS_M_H
-# define ARGS_M_H
+#ifndef ARGS_H
+# define ARGS_H
 
 # include <unistd.h>
 # include <fcntl.h>
@@ -12,16 +12,16 @@
 int		check_args(int argc, char **argv, t_scene *scene);
 int		save(int fd, t_scene *scene);
 void	init_contents_flags(int *flags, t_scene *scene, char *whitespaces);
-int	save_contents(int fd, t_scene *scene, int *err_line);
-int	save_line(t_scene *scene, char **split, int *flags);
-int	save_ambient_light(t_scene *scene, char **split, int *flags);
-int	save_lights(t_scene *scene, char **split, int *flags);
-int	save_camera(t_scene *scene, char **split, int *flags);
-int	save_objs(t_scene *scene, char **split);
-int	is_obj_line(char *str);
-int	save_sp(t_scene *scene, char **split);
-int	save_pl(t_scene *scene, char **split);
-int	save_cy(t_scene *scene, char **split);
-int	save_objs_color(t_object *obj, char *color);
+int		save_contents(int fd, t_scene *scene, int *err_line);
+int		save_line(t_scene *scene, char **split, int *flags);
+int		save_ambient_light(t_scene *scene, char **split, int *flags);
+int		save_lights(t_scene *scene, char **split, int *flags);
+int		save_camera(t_scene *scene, char **split, int *flags);
+int		save_objs(t_scene *scene, char **split);
+int		is_obj_line(char *str);
+int		save_sp(t_scene *scene, char **split);
+int		save_pl(t_scene *scene, char **split);
+int		save_cy(t_scene *scene, char **split);
+int		save_objs_color(t_object *obj, char *color);
 
 #endif
