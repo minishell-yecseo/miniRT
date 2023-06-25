3c745bc59303ecffd24593d69b17d5b32895ffb1
SeoSangmin <seoinseoulcity@gmail.com>
2023-06-25 18:34:08 +0900

    style : miniRT_bonus window name

--- sources/main_bonus.c
+++ sources/main_bonus.c
@@ -13,7 +13,7 @@ int	main(int argc, char **argv)
 	vars.mlx = mlx_init();
 	if (!check_args(argc, argv, &vars))
 		exit(0);
-	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, "miniRT");
+	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, "miniRT_bonus");
 	vars.img.image = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
 	drawing(&(vars.img), &vars);
 	mlx_key_hook(vars.win, key_hook_esc, &vars);
